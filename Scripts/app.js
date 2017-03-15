58b691d1335d38f1a0010b8e07da8041dfcec6d8
john <johnjohnson45@live.com>
2017-03-15 00:57:20 -0400

    slider do not worry about added images

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('construction',['ngMaterial','ngRoute'])
+angular.module('construction',['ngMaterial','ngRoute','jkAngularCarousel'])
 //route tells ng-view what html and controller to use
 .config(['$routeProvider',function ($routeProvider) {
 	$routeProvider
@@ -20,6 +20,11 @@ angular.module('construction',['ngMaterial','ngRoute'])
 }])
 //controller for Home view
 .controller('home',['$scope',function($scope){
+	$scope.data = [ {src:"images/IMG_20170306_092627385.jpg"},
+					{src:"images/IMG_20170306_092632168.jpg"}
+
+	]
+	$scope.windowWidth  = $(window).width();
 
 }])
 //controller for projects view
